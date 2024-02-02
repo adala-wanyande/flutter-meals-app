ddcada24b8ab7bfc6f8aee32343aa2a3036f1467
adala-wanyande <benardwanyande@gmail.com>
2024-02-02 06:21:41 +0300

    feat: set up grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/categories.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -20,6 +21,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const Text('My name Jeff...'));
+    return MaterialApp(theme: theme, home: const CategoriesScreen());
   }
 }
